da9c9b2b6a9c1083650c28aaf0c5688f533eaeef
Lucas Ghizoni <lucasghizoni@gmail.com>
2017-06-04 22:05:44 -0300

    fixing some weird behaviours

--- custom-carousel.js
+++ custom-carousel.js
@@ -15,13 +15,17 @@
 
 	        //creating a reference to ul tag.
 	        this.ul = this.shadowRoot.querySelector('ul');
+	        this.arrowLeft = this.shadowRoot.querySelector('.arrow-left');
+	        this.arrowRight = this.shadowRoot.querySelector('.arrow-right');
 
 	        //adding listeners
-	        this.shadowRoot.querySelector('.arrow-right').addEventListener('click', this._arrowRight.bind(this));
-	        this.shadowRoot.querySelector('.arrow-left').addEventListener('click', this._arrowLeft.bind(this));
+	        this.arrowRight.addEventListener('click', this._arrowRight.bind(this));
+	        this.arrowLeft.addEventListener('click', this._arrowLeft.bind(this));
 		}
 
 		_arrowRight(){
+			this.arrowLeft.classList.remove('arrow-left-disabled');
+
 			this.ul.classList.remove('carousel-reverse');
 			this.lis.forEach( (li) => {
 				li.style.order = li.style.order == 0 ? this.lis.length - 1 : Number(li.style.order) - 1;
@@ -29,9 +33,17 @@
 			this.ul.classList.add('carousel');
 			this.ul.classList.remove('trans');
 			setTimeout(() => this.ul.classList.add('trans'), 50);
+
+			//if its on the last product, then arrowRight is disabled.
+			if(this.lis[9].style.order == 4){
+				this.arrowRight.classList.add('arrow-right-disabled');
+			}
+
 		}
 
 		_arrowLeft(){
+			this.arrowRight.classList.remove('arrow-right-disabled');
+
 			this.ul.classList.remove('carousel');
 			this.lis.forEach( (li) => {
 			li.style.order = li.style.order == this.lis.length - 1 ? 0 : Number(li.style.order) + 1;
@@ -39,6 +51,10 @@
 			this.ul.classList.add('carousel-reverse');
 			this.ul.classList.remove('trans');
 			setTimeout(() => this.ul.classList.add('trans'), 20);
+
+			if(this.lis[0].style.order == 1){
+				this.arrowLeft.classList.add('arrow-left-disabled');
+			}
 		}
 
 		setReference(product){
